bc36ee897e764656753a41af1fce3c1db7439bcc
julianknodt <julianknodt@gmail.com>
2020-05-07 16:44:57 -0700

    Reduced memory usage of memory sparsity
    In order to properly handle a large volume of data I had to switch to only iterators and no
    intermediate vectors.

--- src/bin/random_sparsity.rs
+++ src/bin/random_sparsity.rs
@@ -3,9 +3,9 @@
 use rand::random;
 use sparse_mat::{matrix::Matrix, output::FiniteFloat, util::compute_threshold};
 
-const BOUNDS: [u16; 4] = [1024, 1024, 1024, 1024];
+const BOUNDS: [u8; 4] = [64; 4];
 
-fn random_items(bounds: [u16; 4]) -> impl Iterator<Item = ([u16; 4], FiniteFloat<f32>)> {
+fn random_items(bounds: [u8; 4]) -> impl Iterator<Item = ([u8; 4], FiniteFloat<f32>)> + Clone {
   let [i, j, k, l] = bounds;
   (0..i).flat_map(move |x| {
     (0..j).flat_map(move |y| {
@@ -14,8 +14,8 @@ fn random_items(bounds: [u16; 4]) -> impl Iterator<Item = ([u16; 4], FiniteFloat
   })
 }
 
-fn load_mat(thresh: f32, v: &[([u16; 4], FiniteFloat<f32>)]) -> Matrix<Vec<u8>, u16, FiniteFloat<f32>, 4> {
-  let entries = v.iter().filter_map(|&(i, v)| {
+fn load_mat<I: Iterator<Item=([u8; 4], FiniteFloat<f32>)>>(thresh: f32, iter: I) -> Matrix<Vec<u8>, u8, FiniteFloat<f32>, 4> {
+  let entries = iter.filter_map(|(i, v)| {
     if v.inner().abs() > thresh {
       Some((i, v))
     } else {
@@ -26,8 +26,8 @@ fn load_mat(thresh: f32, v: &[([u16; 4], FiniteFloat<f32>)]) -> Matrix<Vec<u8>, 
 }
 
 fn main() {
-  let examples = (0..1)
-    .map(|_| random_items(BOUNDS).collect::<Vec<_>>())
+  let examples = (0..30)
+    .map(|_| random_items(BOUNDS))
     .collect::<Vec<_>>();
   let thresholds = [
     0.9, 0.5, 0.4, 0.3, 0.2, 0.10, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.015, 0.01,
@@ -36,9 +36,9 @@ fn main() {
   for &t in &thresholds {
     let items = examples
       .iter()
+      .cloned()
       .map(|e| {
-        let abs_thresh = compute_threshold(e.iter().map(|v| v.1), t).inner();
-        let mat = load_mat(abs_thresh, e.as_slice());
+        let mat = load_mat(1.0-t, e);
         format!("{}", mat.nbytes())
       })
       .collect::<Vec<_>>();
